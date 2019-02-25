5c50bdfd6a0090aab7ded2bc83619c375706f50f
nguyen khac bao anh <khacbaoanh.nguyen@gmail.com>
2019-02-25 11:59:29 +0100

    solution of problem 2 with complexity O(N^2)

--- problem2.py
+++ problem2.py
@@ -10,3 +10,33 @@ the expected output would be [2, 3, 6].
 
 Follow-up: what if you can't use division?
 '''
+def multip_array_element(array):
+    cursor = 0
+    new_array = []
+    # initial result of multiply all elements of array, with help of cursor, we can except this index of out array
+    mul_elem = 1
+    # idea is set value of current index egale 1 to ignore this impact on calculation multiply
+    array_copy = array.copy()
+    while cursor < len(array):
+        # absorption of multiply
+        array_copy[cursor] = 1
+
+        for e in array_copy:
+            mul_elem *= e
+
+        new_array.append(mul_elem)
+
+        # for a new loop we need to initiate array and mul_elem, incremente cursor
+        array_copy = array.copy()
+        cursor += 1
+        mul_elem = 1
+    # show the new array as a result
+    print(new_array)
+
+    # this function have O(N^2) complexity
+    
+if __name__ == "__main__":
+    array = [1, 2, 3, 4, 5]
+    multip_array_element(array)
+    array = [3, 2, 1]
+    multip_array_element(array)
